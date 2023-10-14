2b53be28d48ab44824ff1a04915c8bcb259992f9
godokan <117326245+godokan@users.noreply.github.com>
2023-10-14 14:50:51 +0900

    API 서버 작성

---
+++ src/main/java/com/yasu/ccsapi/CcsApiApplication.java
@@ -0,0 +1,13 @@
+package com.yasu.ccsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasu/ccsapi/CcsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasu.ccsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
