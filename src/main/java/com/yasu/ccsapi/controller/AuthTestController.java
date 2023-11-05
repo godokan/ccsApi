e7778afb768e1136c883c415d0fe71d8267f649d
godokan <117326245+godokan@users.noreply.github.com>
2023-11-06 00:09:01 +0900

    테스트 API 업데이트

--- src/main/java/com/yasu/ccsapi/controller/AuthTestController.java
+++ src/main/java/com/yasu/ccsapi/controller/AuthTestController.java
@@ -1,17 +1,33 @@
 package com.yasu.ccsapi.controller;
 
+import com.yasu.ccsapi.security.CryptoManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
 public class AuthTestController {
 
-    @GetMapping("check")
-    public ResponseEntity<Object> check() {
-        return ResponseEntity.status(HttpStatus.OK).body(true);
+    @GetMapping
+    public ResponseEntity<Object> check(@RequestParam String key) throws Exception {
+        if (key == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(true);
+        CryptoManager crypto = new CryptoManager();
+        String text = crypto.decrypt(key, "202155012");
+        return ResponseEntity.status(HttpStatus.OK).body(text);
+    }
+
+    @GetMapping("/make")
+    public String makeit() throws Exception {
+        String key = "kookookoo/mapApi";
+        CryptoManager crypto = new CryptoManager();
+        key = crypto.encrypt(key, "202155012");
+
+
+        return key;
     }
 }
