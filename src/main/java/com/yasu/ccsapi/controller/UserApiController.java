ae71d4645613226a22b514e7f59adb89c1d8efc0
godokan <nonamekoo@naver.com>
2023-11-06 16:30:08 +0900

    API 키 발급 로직 수정

--- src/main/java/com/yasu/ccsapi/controller/UserApiController.java
+++ src/main/java/com/yasu/ccsapi/controller/UserApiController.java
@@ -1,22 +1,44 @@
 package com.yasu.ccsapi.controller;
 
+import com.yasu.ccsapi.DTO.ApiUserDto;
+import com.yasu.ccsapi.Domain.Repository.ApiUserRepository;
 import com.yasu.ccsapi.security.CryptoManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController {
 
+    @Autowired
+    private ApiUserRepository userRepository;
+
     @GetMapping("/issue")
-    public String issueKey(@RequestParam String id, @RequestParam String stdNum, @RequestParam String apiName) throws Exception {
+    public String issueKey(@RequestParam String id, @RequestParam Integer stdNum, @RequestParam String apiName) throws Exception {
+        // API KEY 구조 : {회원ID}/{API이름}
+        // API KEY 에 대한 복호화 키 : {학번}
+
         String issued;
         CryptoManager manager = new CryptoManager();
-        issued = manager.encrypt(id+"/"+apiName,stdNum);
-        System.out.println(issued);
-        System.out.println(manager.decrypt("o1JHR+pePxQfNeiDOQgUVw==:qbk+s40imz9GSDo81ZQXdQ==",stdNum));
+        issued = manager.encrypt(id+"/"+apiName, String.valueOf(stdNum));
+
+        ApiUserDto userDto = ApiUserDto.builder()
+                .api_key(issued)
+                .studNum(stdNum)
+                .build();
+
+        log.info("API KEY ISSUED : "+userDto.getApi_key() + "/" + userDto.getStudNum());
+
+        userRepository.save(userDto.toEntity());
+
         return issued;
     }
+
+
 }
