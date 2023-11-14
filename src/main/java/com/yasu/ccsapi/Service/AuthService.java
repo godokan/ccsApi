40509943aa1166b80fd1d7982630d249e747d62e
godokan <117326245+godokan@users.noreply.github.com>
2023-11-15 02:39:47 +0900

    키 인증 로직

--- src/main/java/com/yasu/ccsapi/Service/AuthService.java
+++ src/main/java/com/yasu/ccsapi/Service/AuthService.java
@@ -1,33 +1,72 @@
 package com.yasu.ccsapi.Service;
 
+import com.yasu.ccsapi.DTO.ErrorDto;
+import com.yasu.ccsapi.Domain.Entity.ApiListEntity;
+import com.yasu.ccsapi.Domain.Entity.ApiUserEntity;
 import com.yasu.ccsapi.Domain.Entity.ApiUserListEntity;
+import com.yasu.ccsapi.Domain.Entity.CcsUserEntity;
+import com.yasu.ccsapi.Domain.Repository.ApiListRepository;
 import com.yasu.ccsapi.Domain.Repository.ApiUserListRepository;
 import com.yasu.ccsapi.Domain.Repository.ApiUserRepository;
+import com.yasu.ccsapi.Domain.Repository.CcsUserRepository;
+import com.yasu.ccsapi.ErrorCords;
 import com.yasu.ccsapi.security.CryptoManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthService {
 
     @Autowired
-    ApiUserRepository apiUserRepository;
+    private CcsUserRepository ccsUserRepository;
     @Autowired
-    ApiUserListRepository userListRepository;
+    private ApiUserRepository apiUserRepository;
+    @Autowired
+    private ApiUserListRepository userListRepository;
+    @Autowired
+    private ApiListRepository apiListRepository;
 
     private CryptoManager cryptoManager;
 
-//    public String decrypt(String key, Integer studNum) throws Exception {
-//        String out = cryptoManager.decrypt(key, String.valueOf(studNum));
-//
-//    }
+    public String decrypt(String key, Integer studNum) {
+        try {
+            return cryptoManager.decrypt(key, String.valueOf(studNum));
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-    public void auth(String key, Integer studNum) {
+    public String auth(String decryptedKey, Integer studNum) {
         // API KEY 구조 : {회원 ID}/{API 이름}
         // API KEY 에 대한 복호화 키 : {학번}
-        String[] strings = key.split("/");
-        userListRepository.findByUserStudNumAndListName(studNum,strings[1]);
-        // TODO : 유저 리스트 조회 해 유효한 키 인지, 어디에 연결 되는 지 조회
+        String[] strings = decryptedKey.split("/");
+
+        // 학번 으로 CCS 계정 조회
+        CcsUserEntity findCcsUser = ccsUserRepository.findByStudNum(studNum);
+        ApiUserEntity findApiUser = apiUserRepository.findByStudNum(studNum);
+        if(findCcsUser==null || findApiUser==null) // 계정 유효성 검사
+            return "ERR_USER_NOT_FOUND";
+        else if(!findCcsUser.getId().equals(strings[0])) // KEY 의 ID와 학번 으로 조회 한 계정의 ID 검사
+            return "ERR_KEY_NOT_MATCH";
+
+        ApiListEntity apiList = apiListRepository.findByName(strings[1]); // API 유효성 검사
+        if (apiList==null)
+            return "ERR_API_NOT_FOUND";
+
+        return "OK";
+    }
+
+    public String getApiName(String decryptedKey) {
+        return apiListRepository.findByName(decryptedKey.split("/")[1]).getName();
+    }
+
+    public List<ErrorDto> makeErrorMsg(ErrorCords cord) {
+        List<ErrorDto> error = new ArrayList<>();
+        error.add(ErrorDto.builder().errorCode(cord).build());
+        return error;
     }
 
 }
--- src/main/java/com/yasu/ccsapi/controller/MapApiController.java
+++ src/main/java/com/yasu/ccsapi/controller/MapApiController.java
@@ -1,12 +1,17 @@
 package com.yasu.ccsapi.controller;
 
 import com.yasu.ccsapi.DTO.ApiListMapDto;
+import com.yasu.ccsapi.DTO.ErrorDto;
+import com.yasu.ccsapi.ErrorCords;
+import com.yasu.ccsapi.Service.AuthService;
 import com.yasu.ccsapi.Service.MapApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,9 +19,38 @@ import java.util.List;
 public class MapApiController {
     @Autowired
     MapApiService mapApiService;
+    @Autowired
+    AuthService authService;
 
     @GetMapping
-    public List<ApiListMapDto> mapList() {
-        return mapApiService.getMapList();
+    public List<?> mapList(@RequestParam(required = false) String key, @RequestParam(required = false) Integer studNum) {
+        if (key == null)
+            return authService.makeErrorMsg(ErrorCords.KEY_NOT_FOUND);
+        else if (studNum == null)
+            return authService.makeErrorMsg(ErrorCords.STUD_NUM_NOT_FOUND);
+
+        String decryptedKey = authService.decrypt(key, studNum);
+        if(decryptedKey == null)
+            return authService.makeErrorMsg(ErrorCords.API_DECRYPT_ERROR);
+
+        String res = authService.auth(decryptedKey, studNum);
+
+        switch (res) {
+            case "OK" -> {
+                return mapApiService.getMapList();
+            }
+            case "ERR_USER_NOT_FOUND" -> {
+                return authService.makeErrorMsg(ErrorCords.USER_NOT_FOUND);
+            }
+            case "ERR_KEY_NOT_MATCH" -> {
+                return authService.makeErrorMsg(ErrorCords.USER_NOT_MATCH);
+            }
+            case "ERR_API_NOT_FOUND" -> {
+                return authService.makeErrorMsg(ErrorCords.API_NOT_MATCH);
+            }
+            default -> {
+                return authService.makeErrorMsg(ErrorCords.SERVER_ERROR);
+            }
+        }
     }
 }
