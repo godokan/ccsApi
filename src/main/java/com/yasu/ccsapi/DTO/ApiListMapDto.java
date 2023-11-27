dd7155c15433b2c4fd5f3b1b34cfa09b5654e506
godokan <117326245+godokan@users.noreply.github.com>
2023-11-28 00:11:39 +0900

    테이블 구조 변경

--- src/main/java/com/yasu/ccsapi/DTO/ApiListMapDto.java
+++ src/main/java/com/yasu/ccsapi/DTO/ApiListMapDto.java
@@ -8,32 +8,26 @@ import lombok.Data;
 public class ApiListMapDto {
     private Integer no;
     private String name;
-    private String properName;
     private Double lat;
     private Double lng;
     private String address;
-    private String placeUrl;
 
     @Builder
-    public ApiListMapDto(Integer no, String name, String properName, Double lat, Double lng, String address, String placeUrl) {
+    public ApiListMapDto(Integer no,String name, Double lat, Double lng, String address) {
         this.no = no;
         this.name = name;
-        this.properName = properName;
         this.lat = lat;
         this.lng = lng;
         this.address = address;
-        this.placeUrl = placeUrl;
     }
 
     public ApiListMapEntity toEntity() {
         return ApiListMapEntity.builder()
                 .no(no)
                 .name(name)
-                .properName(properName)
                 .lat(lat)
                 .lng(lng)
                 .address(address)
-                .placeUrl(placeUrl)
                 .build();
     }
 }
